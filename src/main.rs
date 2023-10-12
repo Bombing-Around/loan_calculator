30fa36b3d12d03bdf917bd10823036a1113aa0a1
Eph Baum <eph_baum@outlook.com>
2023-10-11 21:25:42 -0700

    (feat) add's simple amort flag
    using chatgpt always wants to use this old clap dep
    updating seems to be a completely different api i yet
    to be able to intuit
    
    using the old version does seem to work as expected
    though this isn't quite what I have sketched out in my head
    
    going to play with codellama next

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
+extern crate clap;
+use clap::{Arg, App};
 use std::io;
 
 fn main() {
+    let matches = App::new("Loan Calculator")
+        .arg(Arg::with_name("amortization")
+            .short("a")
+            .long("amortization")
+            .help("Outputs the amortization schedule"))
+        .get_matches();
+
     println!("Loan Calculator");
 
-    // Get principal amount
     let principal = get_input("Enter the principal amount: ");
-
-    // Get annual interest rate
     let interest_rate = get_input("Enter the annual interest rate: ");
-
-    // Get loan duration in months
     let months = get_input("Enter the loan duration in months: ");
-
-    // Calculate monthly interest rate
     let monthly_interest_rate = (interest_rate / 100.0) / 12.0;
-
-    // Calculate monthly payment
     let monthly_payment = principal * monthly_interest_rate / (1.0 - (1.0 + monthly_interest_rate).powf(-months));
 
     println!("Your monthly payment is: {:.2}", monthly_payment);
+
+    if matches.is_present("amortization") {
+        print_amortization_schedule(principal, monthly_interest_rate, months, monthly_payment);
+    }
 }
 
 fn get_input(prompt: &str) -> f64 {
@@ -33,5 +37,20 @@ fn get_input(prompt: &str) -> f64 {
             println!("Please enter a valid number.");
             get_input(prompt)
         },
+    }
+}
+
+fn print_amortization_schedule(principal: f64, monthly_interest_rate: f64, months: f64, monthly_payment: f64) {
+    let mut remaining_balance = principal;
+
+    println!("Month | Payment | Interest | Principal | Remaining Balance");
+
+    for month in 1..=(months as usize) {
+        let interest_payment = remaining_balance * monthly_interest_rate;
+        let principal_payment = monthly_payment - interest_payment;
+        remaining_balance -= principal_payment;
+
+        println!("{:<6} | {:<8.2} | {:<9.2} | {:<10.2} | {:<17.2}",
+            month, monthly_payment, interest_payment, principal_payment, remaining_balance);
     }
 }
